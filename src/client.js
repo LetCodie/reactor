009c08ff689a8fddedc5bcc48cf99f8292e82b0c
radin-reth <radin.reth@gmail.com>
2017-08-03 22:01:26 +0700

    build jumbotron version of JSX react component

--- src/client.js
+++ src/client.js
@@ -1,2 +1,12 @@
-let name="hello world 2.1";
-console.log(name);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const component = <div className="jumbotron jumbotron-fluid text-sm-center" style={{background: 'url(img/moon-1817885_1920.jpg) no-repeat center center', color: 'white'}}>
+  <div className="container">
+    <h1>Building React.js User Interfaces</h1>
+    <p>with Bootstrap and Sass</p>
+    <a className="btn btn-primary" href="#" target="_blank">Visit us!</a>
+  </div>
+</div>
+
+ReactDOM.render(component, document.getElementById('app'));
