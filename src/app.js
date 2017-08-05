56c1f9f87025bec142b406af3124fe5c4f21c1e6
radin-reth <radin.reth@gmail.com>
2017-08-05 22:15:21 +0700

    create reusable container class

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
+import Container from './bootstrap/container';
 
 export default class App extends React.Component {
   render() {
     return <Jumbotron className="-fluid text-sm-center" style={{background: 'url(img/moon-1817885_1920.jpg) no-repeat center center', color: 'white'}}>
-      <div className="container">
+      <Container>
         <h1>Building React.js User Interfaces 1.1</h1>
         <p>with Bootstrap and Sass</p>
         <Button className="-outline-primary" href="#" target="_blank" />
         <Button className="-danger -sm" disabled target="_blank" />
 
         <Button className="-outline-info -lg -block" href="#" target="_blank" />
-      </div>
+      </Container>
     </Jumbotron>;
   }
 }
--- src/bootstrap/jumbotron.js
+++ src/bootstrap/jumbotron.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {classify} from './utils';
+import Container from './container';
 
 export default class Jumbotron extends React.Component {
   render() {
     return <div {...this.props} className={classify('jumbotron', this.props.className)}>
-      { this.props.children }
+      <Container className="blue" style={{color:'red'}}>
+        { this.props.children }
+      </Container>
     </div>;
   }
 }
