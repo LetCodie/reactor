2521ba6be542ae00b3b39171badbb02a3a4ddf98
radin-reth <radin.reth@gmail.com>
2017-08-06 10:14:10 +0700

    build dynamic css with webpack

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
+import './application.css';
 
 export default class App extends React.Component {
   render() {
-    return <Jumbotron containerFluid={false} className="-fluid text-sm-center" style={{background: 'url(img/moon-1817885_1920.jpg) no-repeat center center', color: 'white'}}>
+    return <Jumbotron containerFluid={false} className="-fluid text-sm-center custom">
         <h1>Building React.js User Interfaces 1.1</h1>
         <p>with Bootstrap and Sass</p>
         <Button className="-outline-primary" href="#" target="_blank" />
