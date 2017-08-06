6ddd1dc6a3cd224e45a7d26df9cbe47834fa09b6
radin-reth <radin.reth@gmail.com>
2017-08-06 09:48:55 +0700

    dynamic jumbotron fluid child

--- src/bootstrap/jumbotron.js
+++ src/bootstrap/jumbotron.js
@@ -4,14 +4,18 @@ import Container from './container';
 
 export default class Jumbotron extends React.Component {
   render() {
+    const CSS_NAME = 'jumbotron';
     const props = Object.assign({}, this.props);
+          props.className = classify('jumbotron', props.className);
     const fluid = props.containerFluid;
     delete props.containerFluid;
+    console.log(props.className.includes(CSS_NAME + '-fluid'));
+    const body = props.className.includes(CSS_NAME + '-fluid') ? props.children:<Container fluid={fluid} className="blue" style={{color:'red'}}>
+      { props.children }
+    </Container>;
 
-    return <div {...props} className={classify('jumbotron', props.className)}>
-      <Container fluid={fluid} className="blue" style={{color:'red'}}>
-        { props.children }
-      </Container>
+    return <div {...props}>
+      {body}
     </div>;
   }
 }
