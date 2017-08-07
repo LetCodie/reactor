f5042bb40ab297e7954a8793b25f74ce5fbf4ea3
radin-reth <radin.reth@gmail.com>
2017-08-07 21:53:20 +0700

    animate with react addons

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './application.scss';
 
 export default class App extends React.Component {
   render() {
-    return <Jumbotron containerFluid={false} className="-faluid text-sm-center custom">
+    return <ReactCSSTransitionGroup
+              transitionName="app"
+              transitionAppear={true}
+              transitionAppearTimeout={500}
+              transitionEnterTimeout={500}
+              transitionLeaveTimeout={300}><Jumbotron containerFluid={false} className="-fluid custom">
         <h1>Building React.js User Interfaces 1.1</h1>
         <p>with Bootstrap and Sass</p>
         <Button className="-outline-primary" href="#" target="_blank" />
-        <Button className="-danger -sm" disabled target="_blank" />
+        <Button className="-danger -sm" target="_blank" />
 
         <Button className="-outline-info -lg -block" href="#" target="_blank" />
-    </Jumbotron>;
+    </Jumbotron></ReactCSSTransitionGroup>;
   }
 }
